c5194eb5637e99d4745acc74a90ff66da8a20d74
Luzivan Gois <luzivangois@outlook.com>
2025-01-17 16:36:27 -0300

    Adequação das regras de CORS para aceitar requisições do frontend hospedado na netlify

--- src/main/java/com/advocacia/api/infra/security/CorsConfig.java
+++ src/main/java/com/advocacia/api/infra/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://advocacia-associados.netlify.app/")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
